5cc99ae1bbb8485ebdd1d3cc32da9b56dc7ff939
Andrew Scibek <andrew.scibek@gmail.com>
2021-01-18 19:56:22 -0800

    init

---
+++ README.md
@@ -0,0 +1,60 @@
+# Current Stats #
+
+Name     |   # times run |        # fails |   task/s |  fail/s
+----------|--------------|-----------------|---------|----------------------------
+   1: Pipeline 1 Complete |        63,762 |         0 (0%) |     2898 |    0.00
+   2: Pipeline 1 Missing  |        63,568 |         0 (0%) |     2889 |    0.00
+   3: Pipeline 2 Complete |        64,422 |         0 (0%) |     2928 |    0.00
+   4: Pipeline 2 Missing  |        63,547 |         0 (0%) |     2888 |    0.00
+ Aggregated               |       255,299 |         0 (0%) |    11604 |    0.00
+
+ ---
+
+ |Name                     |    Avg (ms) |        Min |         Max |     Median|
+  |-------------------------|-------------|------------|-------------|-----------|
+ |  1: Pipeline 1 Complete |      155.09 |        134 |         594 |        140|
+  | 2: Pipeline 1 Missing  |       77.26 |         66 |         173 |         69|
+ |  3: Pipeline 2 Complete |      386.26 |        211 |         468 |        360|
+  | 4: Pipeline 2 Missing  |       77.18 |         66 |         173 |         69|
+ |Aggregated               |      174.65 |         66 |         594 |        140|
+
+---
+
+ Name                     |        # reqs |        # fails |    req/s |  fail/s
+  |-------------------------|-------------|------------|-------------|-----------|
+ POST Pipeline 1 Complete |       127,315 |         0 (0%) |     5787 |    0.00
+ POST Pipeline 1 Missing  |        63,568 |         0 (0%) |     2889 |    0.00
+ POST Pipeline 2 Complete |       319,927 |         0 (0%) |    14542 |    0.00
+ POST Pipeline 2 Missing  |        63,547 |         0 (0%) |     2888 |    0.00
+ -------------------------|---------------|----------------|----------|--------
+ Aggregated               |       574,357 |         0 (0%) |    26107 |    0.00
+
+ ---
+
+ Name                     |    Avg (ms) |        Min |        Max |      Median
+ -------------------------|-------------|------------|-----------|----------------
+ POST Pipeline 1 Complete |       77.27 |         30 |         515 |         69
+ POST Pipeline 1 Missing  |       77.25 |         66 |         173 |         69
+ POST Pipeline 2 Complete |       76.86 |          2 |         173 |         69
+ POST Pipeline 2 Missing  |       77.17 |         66 |         173 |         69
+ Aggregated               |       77.03 |          2 |         515 |         69
+
+ ---
+
+ Slowest page load within specified percentile of requests (in ms):
+ ------------------------------------------------------------------------------
+ Name                     |    50% |    75% |    98% |    99% |  99.9% | 99.99%
+ -------------------------|--------|--------|--------|--------|--------|-------
+ POST Pipeline 1 Complete |     69 |     71 |    170 |    170 |    170 |    170
+ POST Pipeline 1 Missing  |     69 |     71 |    170 |    170 |    170 |    170
+ POST Pipeline 2 Complete |     69 |     71 |    170 |    170 |    170 |    170
+ POST Pipeline 2 Missing  |     69 |     71 |    170 |    170 |    170 |    170
+ Aggregated               |     69 |     71 |    170 |    170 |    170 |    170
+ ------------------------------------------------------------------------------
+ Name                     |                                        Status codes
+ -------------------------|----------------------------------------------------
+ POST Pipeline 1 Complete |                                       127,315 [200]
+ POST Pipeline 1 Missing  |                                        63,568 [200]
+ POST Pipeline 2 Complete |                                       319,927 [200]
+ POST Pipeline 2 Missing  |                                        63,547 [200]
+ Aggregated               |                                       574,357 [200]
---
+++ src/main.rs
@@ -0,0 +1,138 @@
+use goose::prelude::*;
+use uuid::Uuid;
+use serde::{Serialize, Deserialize};
+
+#[cfg(feature = "json")]
+use serde_json;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Pipeline {
+    pub pipeline: String,
+    pub service: String,
+    pub events: Vec<String>,
+}
+
+fn main() -> Result<(), GooseError> {
+    GooseAttack::initialize()?
+        // In this example, we only create a single taskset, named "WebsiteUser".
+        .register_taskset(
+            taskset!("push_events")
+                .register_task(task!(nomarch_event_pipeline_1_all_steps).set_name("Pipeline 1 Complete"))
+                .register_task(task!(nomarch_event_pipeline_1_missing_step_1).set_name("Pipeline 1 Missing"))
+                .register_task(task!(nomarch_event_pipeline_2_all_steps).set_name("Pipeline 2 Complete"))
+                .register_task(task!(nomarch_event_pipeline_2_missing_step_1).set_name("Pipeline 2 Missing"))
+        )
+        .set_default(GooseDefault::Host, "http://localhost:8080")?
+        .set_default(GooseDefault::Users, 2000)?
+        .set_default(GooseDefault::HatchRate, "100")?
+        .set_default(GooseDefault::StatusCodes, true)?
+        .execute()?
+        .print();
+
+    Ok(())
+}
+
+
+async fn nomarch_event_pipeline_1_all_steps(user: &GooseUser) -> GooseTaskResult {
+    let tmp = Uuid::new_v4();
+    let uuid = tmp.to_string();
+    let mut request_builder = user.goose_post("/events").await?;
+    let first: Pipeline = Pipeline{
+        pipeline: "pipeline1".into(),
+        service: "step1".into(),
+        events: vec!(uuid.clone())
+    };
+    if let Ok(body) = serde_json::to_value(&first) {
+        let _goose = user.goose_send(request_builder.json(&body), None).await?;
+    }
+
+    let second: Pipeline = Pipeline{
+        pipeline: "pipeline1".into(),
+        service: "step2".into(),
+        events: vec!(uuid.clone())
+    };
+    if let Ok(body) = serde_json::to_value(&second) {
+        request_builder = user.goose_post("/events").await?;
+        let _goose = user.goose_send(request_builder.json(&body), None).await?;
+    }
+    Ok(())
+}
+
+async fn nomarch_event_pipeline_1_missing_step_1(user: &GooseUser) -> GooseTaskResult {
+    let uuid = Uuid::new_v4();
+    let request_builder = user.goose_post("/events").await?;
+    let req: Pipeline = Pipeline{
+        pipeline: "pipeline1".into(),
+        service: "step2".into(),
+        events: vec!(uuid.to_string())
+    };
+    let _goose = user.goose_send(request_builder.json(&req), None).await?;
+    Ok(())
+}
+
+async fn nomarch_event_pipeline_2_all_steps(user: &GooseUser) -> GooseTaskResult {
+    let tmp = Uuid::new_v4();
+    let uuid = tmp.to_string();
+    let mut request_builder = user.goose_post("/events").await?;
+    let second: Pipeline = Pipeline{
+        pipeline: "pipeline2".into(),
+        service: "step2".into(),
+        events: vec!(uuid.clone())
+    };
+    if let Ok(body) = serde_json::to_value(&second) {
+        let _goose = user.goose_send(request_builder.json(&body), None).await?;
+    }
+
+    let third: Pipeline = Pipeline{
+        pipeline: "pipeline2".into(),
+        service: "step3".into(),
+        events: vec!(uuid.clone())
+    };
+    if let Ok(body) = serde_json::to_value(&third) {
+        request_builder = user.goose_post("/events").await?;
+        let _goose = user.goose_send(request_builder.json(&body), None).await?;
+    }
+    
+    let foura: Pipeline = Pipeline{
+        pipeline: "pipeline2".into(),
+        service: "step4a".into(),
+        events: vec!(uuid.clone())
+    };
+    if let Ok(body) = serde_json::to_value(&foura) {
+        request_builder = user.goose_post("/events").await?;
+        let _goose = user.goose_send(request_builder.json(&body), None).await?;
+    }
+    
+    let fourb: Pipeline = Pipeline{
+        pipeline: "pipeline2".into(),
+        service: "step4b".into(),
+        events: vec!(uuid.clone())
+    };
+    if let Ok(body) = serde_json::to_value(&fourb) {
+        request_builder = user.goose_post("/events").await?;
+        let _goose = user.goose_send(request_builder.json(&body), None).await?;
+    }
+    
+    let fifth: Pipeline = Pipeline{
+        pipeline: "pipeline1".into(),
+        service: "step5".into(),
+        events: vec!(uuid.clone())
+    };
+    if let Ok(body) = serde_json::to_value(&fifth) {
+        request_builder = user.goose_post("/events").await?;
+        let _goose = user.goose_send(request_builder.json(&body), None).await?;
+    }
+    Ok(())
+}
+
+async fn nomarch_event_pipeline_2_missing_step_1(user: &GooseUser) -> GooseTaskResult {
+    let uuid = Uuid::new_v4();
+    let request_builder = user.goose_post("/events").await?;
+    let req: Pipeline = Pipeline{
+        pipeline: "pipeline2".into(),
+        service: "step2".into(),
+        events: vec!(uuid.to_string())
+    };
+    let _goose = user.goose_send(request_builder.json(&req), None).await?;
+    Ok(())
+}
